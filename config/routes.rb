01cd95c95228b28423f861d3c73ee90b76bcd5d5
aftabiqbal1 <aftab.iqbal@tecaudex.com>
2022-04-14 12:55:43 +0500

    authenticationFinal

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
 
 get "signup", to: "registrations#new"
 post "signup", to: "registrations#create"
+
+get "signin", to: "sessions#new"
+post "signin", to: "sessions#create"
+
 delete "logout", to: "sessions#destroy"
 
 root "main#index"
