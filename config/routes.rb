b576a8b5b550408bb6272820bb28d2dbee86b391
aftabiqbal1 <102665071+aftabiqbal1@users.noreply.github.com>
2022-04-14 13:02:02 +0500

    Revert "New branch"

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,6 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-get "signup", to: "registrations#new"
-post "signup", to: "registrations#create"
-
-get "signin", to: "sessions#new"
-post "signin", to: "sessions#create"
-
-delete "logout", to: "sessions#destroy"
-
-root "main#index"
+  # Defines the root path route ("/")
+  root "articles#index"
 end
--- db/migrate/20220413113146_remove_password_column_from_user.rb
+++
@@ -1,5 +0,0 @@
-class RemovePasswordColumnFromUser < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :users, :password, :string
-  end
-end
